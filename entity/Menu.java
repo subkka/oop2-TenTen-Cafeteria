7aa6230965499f61a8bbee60a4d83d5cc5ec330b
DESKTOP-2JVLHQE\Park <beeniaa@naver.com>
2024-04-28 19:56:09 +0900

    Feat: 클래스 파일 생성

---
+++ entity/Menu.java
@@ -0,0 +1,19 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Menu {
+    private Date date;
+    List<Meal> meals = new ArrayList<>();
+    AllergyInfo allergyInfo;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
