5a5dfbbdc95d23e49a402f939dc9e30e278bc443
jyna120 <jyna120@naver.com>
2024-04-30 18:19:44 +0900

    Feat: Admin 클래스 생성

--- oop2-tentenCafeteria/src/main/java/com/cafeteria/Admin.java
+++ oop2-tentenCafeteria/src/main/java/com/cafeteria/Admin.java
@@ -1,4 +1,27 @@
 package com.cafeteria;
 
+import com.cafeteria.repository.SalesRepository;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
 public class Admin {
+    @Setter
+    @Getter
+    private String admin_id;
+    private SalesRepository salesRepository;
+
+    public Admin() {}
+
+    // 총 매출 정보 가져오기
+    public int getTotalSales() {
+        return 0;
+    }
+
+    // 원하는 기간의 매출 정보 가져오기
+    public int getSales(LocalDate startDate, LocalDate endDate) {
+        salesRepository.readSalesInfo(startDate, endDate);
+        return 0;
+    }
 }
