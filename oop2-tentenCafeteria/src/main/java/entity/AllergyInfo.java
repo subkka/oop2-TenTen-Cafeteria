26738ba2cb37ba0fcc0fd3519f8739160f913843
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2024-04-30 16:15:06 +0900

    test
    1

--- oop2-tentenCafeteria/src/main/java/entity/AllergyInfo.java
+++ oop2-tentenCafeteria/src/main/java/entity/AllergyInfo.java
@@ -1,8 +1,13 @@
 package entity;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Getter
+@Setter
 public class AllergyInfo {
     List<String> allergens = new ArrayList<>();
 
