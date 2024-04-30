c41daa54e10c1502f43ee57fe6b1506117d34e8c
Yoonjeong-Shin <yjshin124@gmail.com>
2024-04-30 13:44:51 +0900

    Feat:kiosk

--- Kiosk.java
+++ Kiosk.java
@@ -1,2 +1,61 @@
+import entity.AllergyInfo;
+import entity.Customer;
+import entity.Menu;
+import entity.SalesLog;
+import repository.MenuRepository;
+import repository.SalesRepository;
+import java.util.Date;
+import java.util.List;
 public class Kiosk {
+    private Date currentDate;
+    private List<Menu> weeklyMenu;
+    private SalesRepository salesRepository;
+    private MenuRepository menuRepository;
+    public Kiosk(Date currentDate, List<Menu> weeklyMenu, SalesRepository salesRepository, MenuRepository menuRepository) {
+        this.currentDate = currentDate;
+        this.weeklyMenu = weeklyMenu;
+        this.salesRepository = salesRepository;
+        this.menuRepository = menuRepository;
+    }
+    public void displayDailyMenu() {
+        Menu dailyMenu = (Menu) menuRepository.readMenuInfo(currentDate, currentDate);
+        System.out.println("오늘의 식단");
+        System.out.println(dailyMenu);
+    }
+    public void displayWeekMenu() {
+        Date endDate = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000); // 1 week later
+        weeklyMenu = menuRepository.readMenuInfo(currentDate, endDate);
+        System.out.println("주간 식단");
+        for (Menu menu : weeklyMenu) {
+            System.out.println(menu);
+        }
+    }
+    public void displayAllergyInfo(Menu menu) {
+        System.out.println(menu.getMeals() + "의 알러지 요소 : ");
+        System.out.println(menu.getAllergyInfo());
+    }
+    public Customer useCoupon(Customer customer) {
+        if (customer.getCoupon() > 0) {
+            customer.setCoupon(customer.getCoupon() - 1);
+            System.out.println("쿠폰이 사용되었습니다.");
+        } else {
+            System.out.println("쿠폰이 없습니다.");
+        }
+        return customer;
+    }
+    public Customer buyCoupon(Customer customer) {
+
+        customer.setCoupon(customer.getCoupon() + 10);
+        System.out.println("쿠폰 10장을 구매하였습니다.");
+        return customer;
+    }
+    public AllergyInfo compareAllergy(AllergyInfo allergyInfo) {
+        for (Menu menu : weeklyMenu) {
+            AllergyInfo menuAllergyInfo = menu.getAllergyInfo();
+            if (menuAllergyInfo != null && menuAllergyInfo.getAllergens().contains(allergyInfo)) {
+                return allergyInfo;
+            }
+        }
+        return null;
+    }
 }
--- entity/Menu.java
+++ entity/Menu.java
@@ -9,6 +9,22 @@ public class Menu {
     List<Meal> meals = new ArrayList<>();
     AllergyInfo allergyInfo;
 
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
+    public AllergyInfo getAllergyInfo() {
+        return allergyInfo;
+    }
+
+    public void setAllergyInfo(AllergyInfo allergyInfo) {
+        this.allergyInfo = allergyInfo;
+    }
+
     public Date getDate() {
         return date;
     }
